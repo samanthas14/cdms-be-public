4b293eb25b66665fbf5d0fd7943ecd76f3747f4d
Ken Burcham <kenburcham@gmail.com>
2017-10-20 10:52:02 -0700

    public watertemp scripts

---
+++ sample-database/populate_watertemp_public.sql
@@ -0,0 +1,121 @@
+USE CDMS_Rpt;
+INSERT INTO CDMS_Rpt.dbo.Public_WaterTemp
+(DatasetId, 
+SourceId, 
+LocationId, 
+UserId, 
+ActivityTypeId, 
+CreateDate, 
+ActivityId,
+InstrumentId, 
+ActivityDate,
+AccuracyCheckId,
+PostAccuracyCheckId,
+Timezone,
+Description,
+
+ActivityQAStatusId,
+ActivityQAStatusUserId,
+ActivityQAStatusEffDt,
+ActivityQAStatusComments,
+ActivityQAStatusName,
+
+HeaderId, 
+Technicians, 
+Comments, 
+CollectionType, 
+DepthToWater, 
+FieldActivityType, 
+StaticWaterLevel, 
+WeatherConditions, 
+SamplePeriod, 
+SampleTempUnit, 
+DeployTime, 
+HeaderByUserId,
+HeaderEffDt,
+
+DetailId, 
+ReadingDateTime, 
+GMTReadingDateTime, 
+WaterTemperature, 
+WaterTemperatureF, 
+AirTemperature, 
+AirTemperatureF, 
+WaterLevel, 
+TempAToD, 
+BatteryVolts, 
+Conductivity, 
+RowId, 
+RowStatusId, 
+DetailByUserId, 
+QAStatusId, 
+DetailEffDt, 
+PSI, 
+AbsolutePressure,
+WaterYearCalculated)
+
+SELECT
+a.DatasetId, 
+a.SourceId, 
+a.LocationId, 
+a.UserId, 
+a.ActivityTypeId, 
+a.CreateDate, 
+a.Id AS ActivityId,
+a.InstrumentId, 
+a.ActivityDate,
+a.AccuracyCheckId,
+a.PostAccuracyCheckId,
+a.Timezone,
+a.Description,
+
+aq.QAStatusId as ActivityQAStatusId,
+aq.UserId as ActivityQAStatusUserId,
+aq.EffDt as ActivityQAStatusEffDt,
+aq.Comments as ActivityQAStatusComments,
+aq.QAStatusName as ActivityQAStatusName,
+
+h.Id AS HeaderId, 
+h.Technicians, 
+h.Comments, 
+h.CollectionType, 
+h.DepthToWater, 
+h.FieldActivityType, 
+h.StaticWaterLevel, 
+h.WeatherConditions, 
+h.SamplePeriod, 
+h.SampleTempUnit, 
+h.DeployTime, 
+h.ByUserId as HeaderByUserId,
+h.EffDt as HeaderEffDt,
+
+d.Id AS DetailId, 
+d.ReadingDateTime, 
+d.GMTReadingDateTime, 
+d.WaterTemperature, 
+d.WaterTemperatureF, 
+d.AirTemperature, 
+d.AirTemperatureF, 
+d.WaterLevel, 
+d.TempAToD, 
+d.BatteryVolts, 
+d.Conductivity, 
+d.RowId, 
+d.RowStatusId, 
+d.ByUserId AS DetailByUserId, 
+d.QAStatusId, 
+d.EffDt AS DetailEffDt, 
+d.PSI, 
+d.AbsolutePressure,
+year(d.ReadingDateTime) as WaterYearCalculated
+                         
+						 
+FROM          			 CDMS_PROD.dbo.Activities AS a INNER JOIN
+						 CDMS_PROD.dbo.ActivityQAs_VW AS aq ON aq.ActivityId = a.Id INNER JOIN
+                         CDMS_PROD.dbo.WaterTemp_Header_VW AS h ON h.ActivityId = a.Id INNER JOIN
+                         CDMS_PROD.dbo.WaterTemp_Detail_VW AS d ON d.ActivityId = a.Id
+ WHERE aq.QAStatusId = 5; --approved activities only...
+
+                         
+
+						 
