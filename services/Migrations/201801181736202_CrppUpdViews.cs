30642a41d187200180f31fa2f0181d8a00bf892c
georgec <georgeclark@ctuir.org>
2018-01-23 15:02:21 -0800

    Added ActivityId to Header views.

--- services/Migrations/201801181736202_CrppUpdViews.cs
+++ services/Migrations/201801181736202_CrppUpdViews.cs
@@ -26,7 +26,7 @@ GO
 CREATE VIEW [dbo].[CrppContracts_Header_VW]
 AS
 SELECT        Id, ProjectProponent, ByUserId, EffDt, ProjectLead, CostCenter, ProjectName, Client, AgreeNumb, DateExec, 
-DraftDue, FinalDue, ContractEnd, ModExtDate, DocumentLink, ActivityComments
+DraftDue, FinalDue, ContractEnd, ModExtDate, DocumentLink, ActivityComments, ActivityId
 FROM            dbo.CrppContracts_Header AS h
 WHERE        (EffDt =
                              (SELECT        MAX(EffDt) AS MaxEffDt
@@ -40,7 +40,7 @@ CREATE VIEW [dbo].[CrppContracts_VW]
 AS
 SELECT        a.Id AS ActivityId, a.DatasetId, a.SourceId, a.LocationId, a.UserId, a.ActivityTypeId, a.CreateDate, a.ActivityDate, 
 h.Id, h.ProjectProponent, h.ByUserId, h.EffDt, h.ProjectLead, h.CostCenter, h.ProjectName, h.Client, h.AgreeNumb, h.DateExec, 
-h.DraftDue, h.FinalDue, h.ContractEnd, h.ModExtDate, h.DocumentLink, h.ActivityComments,
+h.DraftDue, h.FinalDue, h.ContractEnd, h.ModExtDate, h.DocumentLink, h.ActivityComments, h.ActivityId,
 d.Id AS CrppContracts_Detail_Id, d.Testing, d.NewSites, d.MonitoredSites, d.SitesEvaluated, d.UpdatedSites, d.NewIsolates, 
 d.Evaluation, d.Monitoring, d.Notes, d.RowId, d.RowStatusId, d.ByUserId AS CrppContracts_Detail_ByUserId, d.QAStatusId, 
 d.EffDt AS CrppContracts_Detail_EffDt, d.SurveyAcres, d.TestSites, d.SHRENum, d.SHCSNum, d.HPRCSIT,
