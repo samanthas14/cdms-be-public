e88db5e783952d37bc0884e4db58273df55d9b49
Ken Burcham <kenburcham@gmail.com>
2018-02-08 13:43:41 -0800

    fix: add migration with missing BulkQaChange in config of two datasets

---
+++ services/Migrations/2018020819_HideRowQAStatusForCertainDatasets.sql
@@ -0,0 +1,16 @@
+﻿-- hide bulkqachange field for datasets where it isn't hidden yet.
+-- rule: only watertemp should have bulkqachange not hidden...
+
+--electrofishing
+update datasets set Config = '{"DataEntryPage": {"HiddenFields": ["Instrument","BulkQaChange"]}}' 
+where id in (select d.Id from Datasets d
+ join DataStores ds on d.DatastoreId = ds.Id
+where ds.TablePrefix = 'Electrofishing' );
+
+--crppcontracts
+update datasets set Config = '{"RestrictRoles":["CRPP"], "DataEntryPage": {"HiddenFields": ["Location","ActivityDate","QA","Instrument","BulkQaChange"], "ShowFields": ["DateReceived","ProjectLead"]}}' 
+where id in (select d.Id from Datasets d
+ join DataStores ds on d.DatastoreId = ds.Id
+where ds.TablePrefix = 'CrppContracts' );
+
+
