021e8488137393118f8090c8ca9224f325c49a3f
Ken Burcham <kenburcham@gmail.com>
2018-07-17 09:49:17 -0700

    merge in migrations

--- services/Migrations/201805151804028_AddStreamnetFields.cs
+++ services/Migrations/201805151804028_AddStreamnetFields.cs
@@ -7,6 +7,7 @@ namespace services.Migrations
     {
         public override void Up()
         {
+            return; //disable this migration since these changes are already present in the database
             AddColumn("dbo.StreamNet_NOSA_Detail", "BestValue", c => c.String());
             AddColumn("dbo.StreamNet_NOSA_Detail", "OtherDataSources", c => c.String());
             AddColumn("dbo.StreamNet_SAR_Detail", "BestValue", c => c.String());
