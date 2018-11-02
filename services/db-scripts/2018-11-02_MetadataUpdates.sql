86f11b01e0dd86d16b5c56c6adfcb5ba43117165
Ken Burcham <kenburcham@gmail.com>
2018-11-02 13:20:36 -0700

    metadata updates

--- services/db-scripts/2018-11-02_MetadataUpdates.sql
+++ services/db-scripts/2018-11-02_MetadataUpdates.sql
@@ -1,2 +1,3 @@
 ﻿update metadataproperties set controltype = 'hidden' where id in (11,13,25,26);
-
+update metadataproperties set controltype = 'text' where id in (22);
+update metadataproperties set controltype = 'textarea' where id in (1,3,4,5,9,14,15,20);
