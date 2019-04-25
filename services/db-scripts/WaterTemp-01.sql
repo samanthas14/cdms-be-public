5911524e76b0a03f57c8a27d38d4a9485aa2ec36
Ken Burcham <kenburcham@gmail.com>
2019-04-25 14:08:34 -0700

    cleanup for tribalcdms

--- services/db-scripts/WaterTemp-01.sql
+++
@@ -1,8 +0,0 @@
-
-ALTER TABLE [dbo].[WaterTemp_Detail] ADD [Discharge] [float]
-
-INSERT INTO dbo.Fields(FieldCategoryId, Name, [Description], Units, Validation, DataType, PossibleValues, DbColumnName, ControlType, [Rule]) 
-VALUES
-(2,'Discharge','Volumetric flow rate of water that is transported through a given location. It includes any suspended solids, dissolved chemicals, or biologic material in addition to the water itself',
-    'cfs','[0,5000]','float',null,'Discharge','number',null)
-           
