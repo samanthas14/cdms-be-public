f0fc055d5c08d5a3db1a32494a9d676865b0b905
Ryan Kinzer <ryank@nezperce.org>
2019-10-23 09:40:52 -0600

    added sturgeon and PIT-array datasets

---
+++ services/Models/Data/Arrays_Site_Detail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace services.Models.Data
+{
+    public class Arrays_Site_Detail : DataDetail
+    {
+        public string SiteId { get; set; }
+        public string Estimate { get; set; }
+        public string StDev { get; set; }
+        public string LowerCI { get; set; }
+        public string UpperCI { get; set; }
+    }
+}
---
+++ services/Models/Data/NPT_Sturgeon_Detail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace services.Models.Data
+{
+    public class NPT_Sturgeon_Detail : DataDetail
+    {
+        public string PITCode { get; set; }
+        public string RKM_stream { get; set; }
+        public string RKM_columbia { get; set; }
+        public int? FloyTag { get; set; }
+        public string ForkLength { get; set; }
+        public string TotalLength { get; set; }
+        public string Girth { get; set; }
+        public string Weight { get; set; }
+        public string EventType { get; set; }
+        public string CaptureMethod { get; set; }
+        public string HookType { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string Comments { get; set; }
+    }
+}
