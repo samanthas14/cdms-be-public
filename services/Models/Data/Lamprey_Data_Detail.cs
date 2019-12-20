06e7580e0806a30a38806e9b54e18c9734b332c6
Ryan Kinzer <ryank@nezperce.org>
2019-12-20 09:01:06 -0700

    added lamprey dataset

---
+++ services/Models/Data/Lamprey_Data_Detail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace services.Models.Data
+{
+    public class Lamprey_Data_Detail : DataDetail
+    {
+        public float Length { get; set; }
+        public float Weight { get; set; }
+        public string LifeStage { get; set; }
+        public string Sample { get; set; }
+        public string SampleNumber { get; set; }
+        public string SampleComment { get; set; }
+
+    }
+}
---
+++ services/Models/Data/Lamprey_Data_Header.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace services.Models.Data
+{
+    public class Lamprey_Data_Header : DataHeader
+    {
+
+        public string SpeciesRun { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public float WaterTemperature { get; set; }
+        public float PlotLength { get; set; }
+        public float PlotWidth { get; set; }
+        public string PredominantHabitatType { get; set; }
+        public int FirstPassTime { get; set; }
+        public int FirstPassCount { get; set; }
+        public int SecondPassTime { get; set; }
+        public int SecondPassCount { get; set; }
+        public int ThirdPassTime { get; set; }
+        public int ThirdPassCount { get; set; }
+        public string FishReleasedNoData { get; set; }
+        public string Collector { get; set; }
+        public string SurveyComment { get; set; }
+    }
+}
